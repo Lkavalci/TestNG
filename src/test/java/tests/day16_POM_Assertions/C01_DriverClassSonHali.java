9debac3593bd1da29694715f59a7e34f64613406
lutfi <lutfikavalci@gmail.com>
2023-05-20 19:44:18 +0300

    230520first

---
+++ src/test/java/tests/day16_POM_Assertions/C01_DriverClassSonHali.java
@@ -0,0 +1,63 @@
+package tests.day16_POM_Assertions;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import utilities.Driver;
+
+public class C01_DriverClassSonHali {
+
+    // 3 tane test methodu oluşturalım
+// 1. methodda amazona gidip URL'in amazon içerdiğini test edin
+    @Test
+    public void amazonTest(){
+        Driver.getDriver().get(ConfigReader.getProperty("amazonUrl"));
+
+        String expectedIcerik="amazon";
+        String actualUrl= Driver.getDriver().getCurrentUrl();
+
+        Assert.assertTrue(actualUrl.contains(expectedIcerik));
+        Driver.closeDriver();
+    }
+
+// 2. methodda wise quarter ana sayfaya gidip title'ın wise quarter içerdiğini test edin
+// 3. methodda facebook ana sayfaya gidip, title'ın facebook içerdiğini test edin.
+// her methodda yeni driver oluşturup method sonunda driver ı kapatın
+}
+/*
+package tests.day16_POM_Assertions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import utilities.ConfigReader;
+import utilities.Driver;
+public class C01_DriverClassSonHali {
+    // 3 tane test methodu olusturalim
+    // 1. method'da amazon'a gidip, url'in amazon icerdigini test edin
+    @Test
+    public void amazonTest(){
+        Driver.getDriver().get(ConfigReader.getProperty("amazonUrl"));
+        String expectedIcerik ="amazon";
+        String actualUrl= Driver.getDriver().getCurrentUrl();
+        Assert.assertTrue(actualUrl.contains(expectedIcerik));
+        Driver.closeDriver();
+    }
+    // 2. method'da Wisequarter anasayfaya gidip, title'in Wise Quarter icerdigini test edin
+    @Test
+    public void wiseTest(){
+        Driver.getDriver().get(ConfigReader.getProperty("wiseUrl"));
+        String expectedIcerik= "Wise Quarter";
+        String actualTitle= Driver.getDriver().getTitle();
+        Assert.assertTrue(actualTitle.contains(expectedIcerik));
+        Driver.closeDriver();
+    }
+    // 3. method'da facebook anasayfaya gidip, title'in Facebook icerdigini test edin
+    @Test
+    public void facebookTest(){
+        Driver.getDriver().get(ConfigReader.getProperty("faceUrl"));
+        String expectedIcerik = "Facebook";
+        String actualTitle=Driver.getDriver().getTitle();
+        Assert.assertTrue(actualTitle.contains(expectedIcerik));
+        Driver.closeDriver();
+    }
+    // her method'da yeni driver olusturup, method sonunda driver'i kapatin
+}
+ */
